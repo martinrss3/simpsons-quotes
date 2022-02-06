d974e55eb03ec8b01c8a99178f7c845072bdb444
Martin Russo <martin@MacBook-Pro.local>
2022-02-06 01:21:04 -0300

    Add svg clouds and animate them

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,11 +3,57 @@ import { Link } from "react-router-dom";
 import gsap, { Bounce } from "gsap";
 import FallingBart from "../img/falling-bart.png";
 import CutOutBart from "../img/cutout-bart.png";
+import Cloud1 from "../img/clouds/cloud1.svg";
+import Cloud2 from "../img/clouds/cloud2.svg";
+import Cloud3 from "../img/clouds/cloud3.svg";
+import Cloud4 from "../img/clouds/cloud4.svg";
+import Cloud5 from "../img/clouds/cloud5.svg";
+import Cloud6 from "../img/clouds/cloud6.svg";
 import { Nav } from "../components/Nav";
 import { Footer } from "../components/Footer";
 import "../css/hero.css";
 
 export const Hero = () => {
+  useEffect(() => {
+    gsap.to(".cloud-1", 30, {
+      x: -3000,
+      ease: "none",
+      repeat: -1,
+    });
+
+    gsap.to(".cloud-2", 45, {
+      x: 3000,
+      ease: "none",
+      repeat: -1,
+    });
+
+    gsap.to(".cloud-3", 60, {
+      x: -3000,
+      ease: "none",
+      repeat: -1,
+    });
+
+    gsap.to(".cloud-4", 40, {
+      x: 3000,
+      ease: "none",
+      repeat: -1,
+      delay: 1,
+    });
+
+    gsap.to(".cloud-5", 45, {
+      x: -3000,
+      ease: "none",
+      repeat: -1,
+    });
+
+    gsap.to(".cloud-6", 29, {
+      x: 3000,
+      ease: "none",
+      repeat: -1,
+      delay: 1,
+    });
+  }, []);
+
   useEffect(() => {
     var tl = gsap.timeline();
 
@@ -37,6 +83,14 @@ export const Hero = () => {
       <div className="hero-top-content">
         <div className="hero-navbar">
           <Nav />
+        </div>
+        <div className="clouds-wrapper">
+          <img className="cloud-1" src={Cloud1} alt="cloud" />
+          <img className="cloud-2" src={Cloud2} alt="cloud" />
+          <img className="cloud-3" src={Cloud3} alt="cloud" />
+          <img className="cloud-4" src={Cloud4} alt="cloud" />
+          <img className="cloud-5" src={Cloud5} alt="cloud" />
+          <img className="cloud-6" src={Cloud6} alt="cloud" />
         </div>
         <img className="bart" src={FallingBart} alt="falling bart" />
         <Link to="/search">
